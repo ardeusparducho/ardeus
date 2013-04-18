6496341238e8fd152ee9fe66be69e7cfb4807de4
Ardeus Parducho <ardeus@klab.com>
2013-04-18 16:47:42 +0800

    UIElements Project

---
+++ UIElements/UIElements/UIElementsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIElementsAppDelegate.h
+//  UIElements
+//
+//  Created by Ardeus Joseph R. Parducho  on 4/18/13.
+//  Copyright (c) 2013 Ardeus Joseph R. Parducho . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIElementsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIElements/UIElements/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIElements
+//
+//  Created by Ardeus Joseph R. Parducho  on 4/18/13.
+//  Copyright (c) 2013 Ardeus Joseph R. Parducho . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIElementsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UIElementsAppDelegate class]));
+    }
+}
